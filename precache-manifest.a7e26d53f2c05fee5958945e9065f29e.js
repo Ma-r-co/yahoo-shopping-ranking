3cc0f8a217739500af60e5cfceac3247ed393382
Makoto Watanabe <mktwtnb1989@gmail.com>
2020-03-26 17:26:19 +0000

    Updates

---
+++ precache-manifest.a7e26d53f2c05fee5958945e9065f29e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891e04617f9422c971ade0d0bc0e7d05",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e7b80cc0d87064291fdd",
+    "url": "./static/js/2.7a8fe0dc.chunk.js"
+  },
+  {
+    "revision": "d140213c99598621a8b4e82fad7572a4",
+    "url": "./static/js/2.7a8fe0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee1ecc050989d124c4f",
+    "url": "./static/js/main.ee4f750d.chunk.js"
+  },
+  {
+    "revision": "caa227cc790398b47ae9",
+    "url": "./static/js/runtime-main.f16f9bea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
